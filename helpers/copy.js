c98cb7b2147c5b1e3db68eec600c1e7e17077702
Tim Quigg <timjquigg@gmail.com>
2022-12-15 15:39:34 -0700

    Move method to download input to helper module that creates the new question file

--- helpers/copy.js
+++ helpers/copy.js
@@ -1,12 +1,36 @@
-const { copyFile } = require('fs');
+require('dotenv').config();
+const fs = require('fs').promises;
 const path = require('path');
+
+const {getInput} = require('./getInput');
+
 const day = process.argv.slice(2);
+const cookie = process.env.COOKIE;
 
 ((day) => {
+  // Copy template to new answer file
+  fs.copyFile(path.resolve(__dirname, '../answers/template.js'), path.resolve(__dirname,`../answers/day_${day}.js`))
+    .then(() => {
+      console.log(`template.js was copied to day_${day}.js`);
+    }).catch((err) => {
+      throw err;
+    });
 
-  copyFile(path.resolve(__dirname, '../answers/template.js'), path.resolve(__dirname,`../answers/day_${day}.js`), (err) => {
-    if (err) throw err;
-    console.log(`template.js was copied to day_${day}.js`);
-
-  });
+  // Download input for the day and then write to input file
+  getInput(day, cookie)
+  // Once input is downloaded, save to file then run test
+    .then((response) => {
+      const input = response.data;
+      fs.writeFile(`./input/day_${day}.txt`, input)
+        .then(()=> {
+          console.log(`Input for day ${day} has bee downloaded to /input/day_${day}.txt`);
+        })
+        .catch((err)=>{
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err.response);
+    });
+      
 })(day);
--- index.js
+++ index.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 const fs = require('fs').promises;
 const day = process.argv.slice(2);
 
-const {getInput} = require('./helpers/getInput');
 const {runtest} = require(`./answers/day_${day}.js`);
-
-const cookie = process.env.COOKIE;
 
 fs.readFile(`./input/day_${day}.txt`, {encoding: 'utf8'})
   // If input file exists, run from file
@@ -13,21 +10,6 @@ fs.readFile(`./input/day_${day}.txt`, {encoding: 'utf8'})
     runtest(data);
   })
   // If input file does not exist, download it
-  .catch(() => {
-    console.log(`Input file does not exist for day ${day}.\nDownloading now...................`);
-    getInput(day, cookie)
-      // Once input is downloaded, save to file then run test
-      .then((response) => {
-        const input = response.data;
-        fs.writeFile(`./input/day_${day}.txt`, input)
-          .then(()=> {
-            runtest(input);
-          })
-          .catch((err)=>{
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
+  .catch((err) => {
+    console.log(err);
   });
