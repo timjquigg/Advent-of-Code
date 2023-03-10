c857217a0bff9424994c3e0ac92a5641fd813f4b
Tim Quigg <timjquigg@gmail.com>
2023-03-10 12:58:46 -0700

    Convert day 3 to TypeScript

--- answers/template.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.runtest = void 0;
-const runtest = (input) => {
-    // Code goes here
-};
-exports.runtest = runtest;
--- answers/template.ts
+++ answers/template.ts
@@ -1,3 +1,7 @@
-export const runtest = (input: string) => {
-  // Code goes here
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.runtest = void 0;
+const runtest = (input) => {
+    // Code goes here
 };
+exports.runtest = runtest;
