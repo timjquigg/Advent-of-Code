4736bc290517c3dd2b5facb96cc50e6ff4eabc8e
Tim Quigg <timjquigg@gmail.com>
2023-03-10 13:52:17 -0700

    Convert days 8 & 9 to TypeScript

--- answers/day_9.ts
+++ answers/day_9.ts
@@ -1,20 +1,20 @@
-const key = {
-  'R': [1, 0],
-  'L': [-1, 0],
-  'U': [0, 1],
-  'D': [0, -1]
+const key: { [key: string]: number[] } = {
+  R: [1, 0],
+  L: [-1, 0],
+  U: [0, 1],
+  D: [0, -1],
 };
 
-const moveHead = (input) => {
-  const start = [0,0];
+const moveHead = (input: string[][]): number[][] => {
+  const start = [0, 0];
   const history = [start];
 
   for (let i = 0; i < input.length; i++) {
-    const move = [];
+    const move: number[][] = [];
     for (let j = 0; j < Number(input[i][1]); j++) {
       move.push(key[input[i][0]]);
     }
-    move.forEach(el => {
+    move.forEach((el) => {
       const lastLocation = history.slice(-1)[0];
       history.push([el[0] + lastLocation[0], el[1] + lastLocation[1]]);
     });
@@ -23,19 +23,19 @@ const moveHead = (input) => {
   return history;
 };
 
-const moveTail = (headPositions) => {
-  const start = [0,0];
+const moveTail = (headPositions: number[][]): number[][] => {
+  const start = [0, 0];
   const tailPositions = [start];
 
   // Loop through head positions:
   for (let i = 0; i < headPositions.length; i++) {
     const tailLocation = tailPositions.slice(-1)[0];
-    let newLocation;
-    
+    let newLocation: number[] = [];
+
     // Compare distance between head & tail
     const diffX = headPositions[i][0] - tailLocation[0];
     const diffY = headPositions[i][1] - tailLocation[1];
-    
+
     if (Math.abs(diffX) <= 1 && Math.abs(diffY) <= 1) {
       // console.log('No changes needed');
       continue;
@@ -46,20 +46,23 @@ const moveTail = (headPositions) => {
       const changeX = diffX / Math.abs(diffX);
       newLocation = [tailLocation[0] + changeX, tailLocation[1]];
     }
-    
+
     if (Math.abs(diffY) > 1 && diffX === 0) {
       // console.log('Y difference too big');
       const changeY = diffY / Math.abs(diffY);
       newLocation = [tailLocation[0], tailLocation[1] + changeY];
     }
 
-    if ((Math.abs(diffX) > 1 && Math.abs(diffY) >= 1) || (Math.abs(diffY) > 1 && Math.abs(diffX) >= 1)) {
+    if (
+      (Math.abs(diffX) > 1 && Math.abs(diffY) >= 1) ||
+      (Math.abs(diffY) > 1 && Math.abs(diffX) >= 1)
+    ) {
       // console.log('Diagonal');
       const changeX = diffX / Math.abs(diffX);
       const changeY = diffY / Math.abs(diffY);
       newLocation = [tailLocation[0] + changeX, tailLocation[1] + changeY];
     }
-    
+
     // Add new tail position to array
     tailPositions.push(newLocation);
   }
@@ -67,18 +70,17 @@ const moveTail = (headPositions) => {
   return tailPositions;
 };
 
-const getUnique = (positions) => {
-  
-  const uniqueObj = {};
+const getUnique = (positions: number[][]): number[][] => {
+  const uniqueObj: { [key: string]: number[] } = {};
 
   // Iteration
   for (let i = 0; i < positions.length; i++) {
     // The current array, from the array of arrays
     const currentPosition = positions[i];
- 
+
     // The JSON stringified version of the current array
     const stringified = JSON.stringify(currentPosition);
- 
+
     // add current to obj, if current position already there nothing will happen
     uniqueObj[stringified] = currentPosition;
   }
@@ -88,12 +90,14 @@ const getUnique = (positions) => {
   return results;
 };
 
-const runtest = (input) => {
-  
-  input = input.split('\n').map(el => el.split(' ')).slice(0,-1);
-  
+export const runtest = (inputStr: string) => {
+  const input: string[][] = inputStr
+    .split("\n")
+    .map((el) => el.split(" "))
+    .slice(0, -1);
+
   const numberOfTails = 9;
-  
+
   const headPositions = moveHead(input);
   const tailPositions = [];
   for (let i = 0; i < numberOfTails; i++) {
@@ -106,11 +110,13 @@ const runtest = (input) => {
 
   // Part 1
   const oneTailUniquePositions = getUnique(tailPositions[0]);
-  console.log(`Number of unique locations visited by the tail: ${oneTailUniquePositions.length}`);
-  
+  console.log(
+    `Number of unique locations visited by the tail: ${oneTailUniquePositions.length}`
+  );
+
   // Part 2
   const nineTailsUniqueTailPositions = getUnique(tailPositions.slice(-1)[0]);
-  console.log(`Number of unique locations visited by the tail: ${nineTailsUniqueTailPositions.length}`);
+  console.log(
+    `Number of unique locations visited by the tail: ${nineTailsUniqueTailPositions.length}`
+  );
 };
-
-module.exports = {runtest};
